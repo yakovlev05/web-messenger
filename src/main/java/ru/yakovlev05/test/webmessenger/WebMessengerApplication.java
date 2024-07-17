58b79f43e64580834e9962797ad1051c232a3774
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-07-17 13:25:43 +0500

    create project

---
+++ src/main/java/ru/yakovlev05/test/webmessenger/WebMessengerApplication.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.test.webmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yakovlev05/test/webmessenger/WebMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.test.webmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
