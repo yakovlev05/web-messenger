6a3cd6d47363cbbbcb87d579c1e3852e4e73c2a2
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-07-23 22:02:37 +0500

    add SwaggerConfig

---
+++ web-messenger-backend/src/main/java/ru/yakovlev05/test/webmessenger/configs/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package ru.yakovlev05.test.webmessenger.configs;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Web Messenger API"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("TEST"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .name("JWT auth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
