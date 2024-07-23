1c48869d05fcdd71a0f1cef3fb440ec38450d2d9
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-07-23 18:04:34 +0500

    add UserDetailsImpl

---
+++ web-messenger-backend/src/main/java/security/UserDetailsImpl.java
@@ -0,0 +1,36 @@
+package security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.yakovlev05.test.webmessenger.entity.enums.Role;
+
+import java.util.Collection;
+import java.util.Set;
+
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private String username;
+    private String password;
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(x -> new SimpleGrantedAuthority(x.name()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
+
