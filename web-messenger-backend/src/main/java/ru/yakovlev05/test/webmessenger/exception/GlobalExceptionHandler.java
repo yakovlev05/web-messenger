1b0fff14ef459dd6c694ce71666a60bdb16ab997
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-07-25 00:47:28 +0500

    add GlobalExceptionHandler

---
+++ web-messenger-backend/src/main/java/ru/yakovlev05/test/webmessenger/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package ru.yakovlev05.test.webmessenger.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.yakovlev05.test.webmessenger.dto.MessageDto;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<MessageDto> handleException(Exception exception) {
+        exception.printStackTrace();
+        return new ResponseEntity<>(new MessageDto(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
