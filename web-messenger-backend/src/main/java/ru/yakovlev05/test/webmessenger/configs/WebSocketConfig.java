07c21387be990a3dbd60777401abc92ac3859065
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-07-26 20:39:21 +0500

    add JwtAuthChannelInterceptor

--- web-messenger-backend/src/main/java/ru/yakovlev05/test/webmessenger/configs/WebSocketConfig.java
+++ web-messenger-backend/src/main/java/ru/yakovlev05/test/webmessenger/configs/WebSocketConfig.java
@@ -1,22 +1,29 @@
 package ru.yakovlev05.test.webmessenger.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import ru.yakovlev05.test.webmessenger.filter.JwtAuthChannelInterceptor;
 
 import java.util.List;
 
 // Хороший проект для ознакомления - https://github.com/spring-guides/gs-messaging-stomp-websocket
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtAuthChannelInterceptor jwtAuthChannelInterceptor;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 
@@ -60,5 +67,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         converter.setContentTypeResolver(resolver);
         messageConverters.add(converter);
         return false;
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        // Регистрируем интерсептор для обработки входящих сообщений
+        // Он необходим для авторизации в вебсокетах
+        // Его логика аналогична фильтру для хттп запросов
+        registration.interceptors(jwtAuthChannelInterceptor);
     }
 }
