7ec93a6d1fda867be95a08f46879a8321f8fe4db
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-09-03 22:13:58 +0500

    fix swagger with https connection

--- web-messenger-backend/src/main/java/ru/yakovlev05/test/webmessenger/configs/SwaggerConfig.java
+++ web-messenger-backend/src/main/java/ru/yakovlev05/test/webmessenger/configs/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +25,7 @@ public class SwaggerConfig {
                                 .name("JWT auth")
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
-                                .bearerFormat("JWT")));
+                                .bearerFormat("JWT")))
+                .addServersItem(new Server().url("/"));
     }
 }
